c43376ef8e1860406fe2958b081e3fccfd18d96a
wallauerr <wallauer@protonmail.com>
2025-06-01 23:38:51 -0300

    first commit

---
+++ test/phoenix_bank_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PhoenixBankWeb.ErrorJSONTest do
+  use PhoenixBankWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PhoenixBankWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PhoenixBankWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
