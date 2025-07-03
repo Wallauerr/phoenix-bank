bd79a3ea6a6729ea614967b135fd9dfc1001ec71
wallauerr <wallauer@protonmail.com>
2025-07-02 23:25:27 -0300

    feat: Add Users.Get module to fetch user by ID
    Handle not found case by returning {:error, :not_found}

---
+++ lib/phoenix_bank/users/get.ex
@@ -0,0 +1,11 @@
+defmodule PhoenixBank.Users.Get do
+  alias PhoenixBank.Repo
+  alias PhoenixBank.Users.User
+
+  def call(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+end
