b0b212574a609873b4b527f11d766d7eda4cbfae
wallauerr <wallauer@protonmail.com>
2025-07-02 23:27:43 -0300

    feat: add not found user error response

--- lib/phoenix_bank_web/controllers/error_json.ex
+++ lib/phoenix_bank_web/controllers/error_json.ex
@@ -3,6 +3,10 @@ defmodule PhoenixBankWeb.ErrorJSON do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
+  def error(%{status: :not_found}) do
+    %{status: :not_found, message: "User not found"}
+  end
+
   def error(%{changeset: changeset}) do
     %{errors: Ecto.Changeset.traverse_errors(changeset, &translate_error/1)}
   end
