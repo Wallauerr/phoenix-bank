1cb8ddfcfd8b61eeea2b3718d1a47b6c12f1f98d
wallauerr <wallauer@protonmail.com>
2025-07-02 23:28:17 -0300

    feat: handle not found errors in FallbackController
    Add case to return 404 status code and render error JSON when
    encountering :not_found errors

--- lib/phoenix_bank_web/controllers/fallback_controller.ex
+++ lib/phoenix_bank_web/controllers/fallback_controller.ex
@@ -1,6 +1,13 @@
 defmodule PhoenixBankWeb.FallbackController do
   use PhoenixBankWeb, :controller
 
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(json: PhoenixBankWeb.ErrorJSON)
+    |> render(:error, status: :not_found)
+  end
+
   def call(conn, {:error, changeset}) do
     conn
     |> put_status(:bad_request)
