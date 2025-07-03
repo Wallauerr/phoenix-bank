8296ceaacc5f9b63191fc59f579136889738921a
wallauerr <wallauer@protonmail.com>
2025-07-02 23:27:14 -0300

    feat: delegate user retrieval to Get module
    The Users module now delegates `get/1` function to Users.Get.call/1

--- lib/phoenix_bank/users.ex
+++ lib/phoenix_bank/users.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixBank.Users do
   alias PhoenixBank.Users.Create
+  alias PhoenixBank.Users.Get
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate get(id), to: Get, as: :call
 end
