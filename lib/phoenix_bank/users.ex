c40ce7137e569de00498556bbb178f9153693c6e
wallauerr <wallauer@protonmail.com>
2025-10-21 00:00:23 -0300

    Add update delegate to Users module

--- lib/phoenix_bank/users.ex
+++ lib/phoenix_bank/users.ex
@@ -1,7 +1,9 @@
 defmodule PhoenixBank.Users do
   alias PhoenixBank.Users.Create
   alias PhoenixBank.Users.Get
+  alias PhoenixBank.Users.Update
 
   defdelegate create(params), to: Create, as: :call
   defdelegate get(id), to: Get, as: :call
+  defdelegate update(params), to: Update, as: :call
 end
