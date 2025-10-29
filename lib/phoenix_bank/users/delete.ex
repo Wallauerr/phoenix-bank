58c1d5aaba513e8feaf2186c45d8f113bfba8aba
wallauerr <wallauer@protonmail.com>
2025-10-28 23:22:24 -0300

    Add PhoenixBank.Users.Delete module

---
+++ lib/phoenix_bank/users/delete.ex
@@ -0,0 +1,11 @@
+defmodule PhoenixBank.Users.Delete do
+  alias PhoenixBank.Repo
+  alias PhoenixBank.Users.User
+
+  def call(%{"id" => id}) do
+    case Repo.get(User, id) do
+      nil -> {:error, :not_found}
+      user -> Repo.delete(user)
+    end
+  end
+end
