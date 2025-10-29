25dc370c9f9dc87e7fcf1301756d79bef8282678
wallauerr <wallauer@protonmail.com>
2025-10-28 23:22:44 -0300

    Add delete function to users

--- lib/phoenix_bank/users.ex
+++ lib/phoenix_bank/users.ex
@@ -1,9 +1,11 @@
 defmodule PhoenixBank.Users do
   alias PhoenixBank.Users.Create
+  alias PhoenixBank.Users.Delete
   alias PhoenixBank.Users.Get
   alias PhoenixBank.Users.Update
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate delete(id), to: Delete, as: :call
   defdelegate get(id), to: Get, as: :call
   defdelegate update(params), to: Update, as: :call
 end
