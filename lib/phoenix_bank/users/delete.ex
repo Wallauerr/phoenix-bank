fded6e9cccb63e0a02ea09b8c40e37a200661428
wallauerr <wallauer@protonmail.com>
2025-10-29 22:47:29 -0300

    Accept id directly in delete call

--- lib/phoenix_bank/users/delete.ex
+++ lib/phoenix_bank/users/delete.ex
@@ -2,7 +2,7 @@ defmodule PhoenixBank.Users.Delete do
   alias PhoenixBank.Repo
   alias PhoenixBank.Users.User
 
-  def call(%{"id" => id}) do
+  def call(id) do
     case Repo.get(User, id) do
       nil -> {:error, :not_found}
       user -> Repo.delete(user)
