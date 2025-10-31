a5701d5dc604234628582673869268921d18432f
wallauerr <wallauer@protonmail.com>
2025-10-30 23:02:00 -0300

    Create test for delete user route

--- test/phoenix_bank_web/users_controller_test.exs
+++ test/phoenix_bank_web/users_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule PhoenixBankWeb.UsersControllerTest do
   use PhoenixBankWeb.ConnCase
 
+  alias PhoenixBank.Users
+  alias Users.User
+
   describe "create/2" do
     test "creates the user", %{conn: conn} do
       params = %{
@@ -41,6 +44,35 @@ defmodule PhoenixBankWeb.UsersControllerTest do
 
       expected_response = %{
         "errors" => %{"cep" => ["should be 8 character(s)"], "name" => ["can't be blank"]}
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "deletes the user", %{conn: conn} do
+      params = %{
+        name: "John Doe",
+        cep: "12345678",
+        email: "john@example.com",
+        password: "123456"
+      }
+
+      {:ok, %User{id: id}} = Users.create(params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{id}")
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{
+          "id" => id,
+          "name" => "John Doe",
+          "cep" => "12345678",
+          "email" => "john@example.com"
+        }
       }
 
       assert response == expected_response
