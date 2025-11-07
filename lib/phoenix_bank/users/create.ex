f4637e7212da12dbb6b6e2ac6293d66344ebd982
wallauerr <wallauer@protonmail.com>
2025-11-06 23:47:55 -0300

    Add create users if cep is valid

--- lib/phoenix_bank/users/create.ex
+++ lib/phoenix_bank/users/create.ex
@@ -1,10 +1,13 @@
 defmodule PhoenixBank.Users.Create do
   alias PhoenixBank.Repo
   alias PhoenixBank.Users.User
+  alias PhoenixBank.ViaCep.Client, as: ViaCepClient
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+  def call(%{"cep" => cep} = params) do
+    with {:ok, _result} <- ViaCepClient.call(cep) do
+      params
+      |> User.changeset()
+      |> Repo.insert()
+    end
   end
 end
