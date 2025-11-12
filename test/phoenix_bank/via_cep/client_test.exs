67348137e363705a5471c8e1bdb8c684aa86b1eb
wallauerr <wallauer@protonmail.com>
2025-11-11 23:11:15 -0300

    Mock body with expected cep

--- test/phoenix_bank/via_cep/client_test.exs
+++ test/phoenix_bank/via_cep/client_test.exs
@@ -12,15 +12,36 @@ defmodule PhoenixBank.ViaCep.ClientTest do
     test "succesfully return cep info", %{bypass: bypass} do
       cep = "93330370"
 
+      body = ~s{
+        "bairro": "Liberdade",
+        "cep": "93330-370",
+        "complemento": "",
+        "ddd": "51",
+        "estado": "Rio Grande do Sul",
+        "gia": "",
+        "ibge": "4313409",
+        "localidade": "Novo Hamburgo",
+        "logradouro": "Rua Corumbá",
+        "regiao": "Sul",
+        "siafi": "8771",
+        "uf": "RS",
+        "unidade": ""
+      }
+
       expected_response = "test"
 
       Bypass.expect(bypass, fn conn ->
-        Plug.Conn.resp(conn, 200, "")
+        Plug.Conn.resp(conn, 200, body)
       end)
 
-      response = Client.call(cep)
+      response =
+        bypass.port
+        |> endpoint_url()
+        |> Client.call(cep)
 
       assert response == expected_response
     end
   end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}/"
 end
