47839ce9737896fbab481597a6751333491f2f20
wallauerr <wallauer@protonmail.com>
2025-11-13 23:25:25 -0300

    configure mox in test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
+Mox.defmock(PhoenixBank.ViaCep.ClientMock, for: PhoenixBank.ViaCep.ClientBehaviour)
+Application.put_env(:phoenix_bank, :via_cep_client, PhoenixBank.ViaCep.ClientMock)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(PhoenixBank.Repo, :manual)
