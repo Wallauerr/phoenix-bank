98b6503ca2c1bbc14358268f2c8c0b76ff401fe6
wallauerr <wallauer@protonmail.com>
2025-11-13 23:26:39 -0300

    create defp client to define dev or test mocked params

--- lib/phoenix_bank/users/create.ex
+++ lib/phoenix_bank/users/create.ex
@@ -4,10 +4,14 @@ defmodule PhoenixBank.Users.Create do
   alias PhoenixBank.ViaCep.Client, as: ViaCepClient
 
   def call(%{"cep" => cep} = params) do
-    with {:ok, _result} <- ViaCepClient.call(cep) do
+    with {:ok, _result} <- client().call(cep) do
       params
       |> User.changeset()
       |> Repo.insert()
     end
+  end
+
+  defp client() do
+    Application.get_env(:phoenix_bank, :via_cep_client, ViaCepClient)
   end
 end
