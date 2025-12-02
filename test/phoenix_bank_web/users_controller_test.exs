d49cbc47b47a8a2363783f57d20c9f688bc33053
Vinicius Wallauer <wallauer@protonmail.com>
2025-12-01 23:28:47 -0300

    create setup for te params and body

--- test/phoenix_bank_web/users_controller_test.exs
+++ test/phoenix_bank_web/users_controller_test.exs
@@ -6,34 +6,36 @@ defmodule PhoenixBankWeb.UsersControllerTest do
   alias PhoenixBank.Users
   alias Users.User
 
-  setup :verify_on_exit!
+  setup do
+    params = %{
+      "name" => "John Doe",
+      "cep" => "93330370",
+      "email" => "john@example.com",
+      "password" => "123456"
+    }
+
+    body = %{
+      "bairro" => "Liberdade",
+      "cep" => "93330-370",
+      "complemento" => "",
+      "ddd" => "51",
+      "estado" => "Rio Grande do Sul",
+      "gia" => "",
+      "ibge" => "4313409",
+      "localidade" => "Novo Hamburgo",
+      "logradouro" => "Rua Corumbá",
+      "regiao" => "Sul",
+      "siafi" => "8771",
+      "uf" => "RS",
+      "unidade" => ""
+    }
+
+    {:ok, %{user_params: params, body: body}}
+  end
 
   describe "create/2" do
-    test "creates the user", %{conn: conn} do
-      params = %{
-        "name" => "John Doe",
-        "cep" => "93330370",
-        "email" => "john@example.com",
-        "password" => "123456"
-      }
-
-      body = %{
-        "bairro" => "Liberdade",
-        "cep" => "93330-370",
-        "complemento" => "",
-        "ddd" => "51",
-        "estado" => "Rio Grande do Sul",
-        "gia" => "",
-        "ibge" => "4313409",
-        "localidade" => "Novo Hamburgo",
-        "logradouro" => "Rua Corumbá",
-        "regiao" => "Sul",
-        "siafi" => "8771",
-        "uf" => "RS",
-        "unidade" => ""
-      }
-
-      expect(PhoenixBank.ViaCep.ClientMock, :call, fn "12345678" ->
+    test "creates the user", %{conn: conn, user_params: params, body: body} do
+      expect(PhoenixBank.ViaCep.ClientMock, :call, fn "93330370" ->
         {:ok, body}
       end)
 
@@ -46,7 +48,7 @@ defmodule PhoenixBankWeb.UsersControllerTest do
                "data" => %{
                  "id" => _id,
                  "name" => "John Doe",
-                 "cep" => "12345678",
+                 "cep" => "93330370",
                  "email" => "john@example.com"
                },
                "message" => "User created successfully"
@@ -79,30 +81,7 @@ defmodule PhoenixBankWeb.UsersControllerTest do
   end
 
   describe "delete/2" do
-    test "deletes the user", %{conn: conn} do
-      params = %{
-        "name" => "John Doe",
-        "cep" => "93330370",
-        "email" => "john@example.com",
-        "password" => "123456"
-      }
-
-      body = %{
-        "bairro" => "Liberdade",
-        "cep" => "93330-370",
-        "complemento" => "",
-        "ddd" => "51",
-        "estado" => "Rio Grande do Sul",
-        "gia" => "",
-        "ibge" => "4313409",
-        "localidade" => "Novo Hamburgo",
-        "logradouro" => "Rua Corumbá",
-        "regiao" => "Sul",
-        "siafi" => "8771",
-        "uf" => "RS",
-        "unidade" => ""
-      }
-
+    test "deletes the user", %{conn: conn, user_params: params, body: body} do
       expect(PhoenixBank.ViaCep.ClientMock, :call, fn "93330370" ->
         {:ok, body}
       end)
