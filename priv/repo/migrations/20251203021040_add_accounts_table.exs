6cfabb5ee6122e2fd270c01f06a523e4b6137241
Vinicius Wallauer <wallauer@protonmail.com>
2025-12-02 23:13:57 -0300

    initial struct of the add accounts table

--- priv/repo/migrations/20251203021040_add_accounts_table.exs
+++ priv/repo/migrations/20251203021040_add_accounts_table.exs
@@ -2,6 +2,9 @@ defmodule PhoenixBank.Repo.Migrations.AddAccountsTable do
   use Ecto.Migration
 
   def change do
+    add :balance, :decimal
+    add :user_id, references(:users)
 
+    timestamps()
   end
 end
