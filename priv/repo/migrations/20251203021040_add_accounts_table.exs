a0e642492de5a61bf8bcc38ea9157149756ae237
Vinicius Wallauer <wallauer@protonmail.com>
2025-12-02 23:27:49 -0300

    move constraint inside to the change

--- priv/repo/migrations/20251203021040_add_accounts_table.exs
+++ priv/repo/migrations/20251203021040_add_accounts_table.exs
@@ -8,7 +8,7 @@ defmodule PhoenixBank.Repo.Migrations.AddAccountsTable do
 
       timestamps()
     end
-  end
 
-  create constraint("accounts", "balance_must_be_positive", check: "balance >= 0")
+    create constraint("accounts", "balance_must_be_positive", check: "balance >= 0")
+  end
 end
