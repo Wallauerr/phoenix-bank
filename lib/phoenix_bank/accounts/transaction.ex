02cae2b25fd8f621e5850ceca708277aeaa32727
Vinicius Wallauer <wallauer@protonmail.com>
2026-02-25 21:58:14 -0300

    add account validation in transaction

--- lib/phoenix_bank/accounts/transaction.ex
+++ lib/phoenix_bank/accounts/transaction.ex
@@ -7,6 +7,10 @@ defmodule PhoenixBank.Accounts.Transaction do
   alias PhoenixBank.Repo
 
   def call(from_account_id, to_account_id, value) do
-    with {}
+    with {:ok, %Account{} = from_account} <- Repo.get(Account, from_account_id),
+         {:ok, %Account{} = to_account} <- Repo.get(Account, to_account_id) do
+    else
+      {:error, :not_found}
+    end
   end
 end
