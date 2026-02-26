05114b0baa322b5faea0bbd7b5c6cdb9f4d884f0
Vinicius Wallauer <wallauer@protonmail.com>
2026-02-25 21:50:50 -0300

    fix alias

--- lib/phoenix_bank/accounts/transaction.ex
+++ lib/phoenix_bank/accounts/transaction.ex
@@ -2,9 +2,9 @@ defmodule PhoenixBank.Accounts.Transaction do
   import Ecto.Multi
   import Decimal
 
-  alias PhoenixBank.Accounts.Account
-  alias PhoenixBank.Repo
+  alias PhoenixBank.Accounts
   alias Accounts.Account
+  alias PhoenixBank.Repo
 
   def call(from_account_id, to_account_id, value) do
     with {}
